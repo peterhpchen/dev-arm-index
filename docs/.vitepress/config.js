8be285ac90341ea4d6c2cb0bb2fcc2fceb17bc2b
peterhpchen <peterhsinpingchen@gmail.com>
2023-01-06 14:20:23 +0800

    chore(config): change repo name

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,62 @@
 export default {
-  base: '/dev-arms-intro/'
-}
+  title: '兵器譜',
+  description: '我的軟體開發工具列表',
+  lang: 'zh-Hant',
+  base: '/my-dev-arms/',
+  themeConfig: {
+    nav: [
+      {
+        text: '作業系統',
+        items: [
+          {
+            text: '跨平台',
+            link: '/os/cross-platform',
+          },
+          {
+            text: 'macOS',
+            link: '/os/macos',
+          },
+          {
+            text: 'Linux',
+            link: '/os/linux',
+          },
+        ],
+      },
+      {
+        text: '終端',
+        items: [
+          {
+            text: '跨 Shell',
+            link: '/terminal/cross-shell',
+          },
+          {
+            text: 'Z shell',
+            link: '/terminal/zsh',
+          },
+        ],
+      },
+      {
+        text: '編輯器',
+        items: [
+          {
+            text: 'Neovim',
+            link: '/editor/nvim',
+          },
+          {
+            text: 'Visual Studio Code',
+            link: '/editor/vscode',
+          },
+        ],
+      },
+      {
+        text: '專案',
+        items: [
+          {
+            text: 'Web',
+            link: '/proj/web',
+          },
+        ],
+      },
+    ],
+  },
+};

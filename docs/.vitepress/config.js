cc74f1fec129f845e6ad3542ba34501f87db59f7
peterhpchen <peterhsinpingchen@gmail.com>
2023-02-04 14:16:51 +0800

    fix(cover): path error

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -13,87 +13,87 @@ export default {
         text: 'Cheatsheet',
         items: [{ text: '總表', link: '/cheatsheet/' }],
       },
-      {
-        text: '桌面應用程式',
-        items: [
-          { text: 'Rectangle - 視窗管理工具', link: '/desktop/rectangle/' },
-        ],
-      },
-      {
-        text: '終端',
-        items: [
-          { text: 'Homebrew - 套件管理工具', link: '/terminal/homebrew/' },
-          { text: 'NVM - Node Version Manager', link: '/terminal/nvm/' },
-          // {
-          //   text: 'Neofetch',
-          //   link: '/terminal/neofetch/',
-          // },
-          // {
-          //   text: 'tmux - 終端管理工具',
-          //   link: '/terminal/tmux/',
-          //   items: [
-          //     {
-          //       text: 'tmuxinator - tmux sessions 管理工具',
-          //       link: '/terminal/tmux/tmuxinator/',
-          //     },
-          //     {
-          //       text: 'TPM - Tmux Plugin Manager',
-          //       link: '/terminal/tmux/tpm/',
-          //     },
-          //   ],
-          // },
-          // {
-          //   text: 'Z shell - Zsh',
-          //   link: '/terminal/zsh/',
-          //   items: [
-          //     {
-          //       text: 'Powerlevel10k',
-          //       link: '/terminal/zsh/powerlevel10k/',
-          //     },
-          //     {
-          //       text: 'zsh-autosuggestions',
-          //       link: '/terminal/zsh/zsh-autosuggestions/',
-          //     },
-          //     {
-          //       text: 'zsh-completions',
-          //       link: '/terminal/zsh/zsh-completions/',
-          //     },
-          //     {
-          //       text: 'zsh-history-substring-search',
-          //       link: '/terminal/zsh/zsh-history-substring-search/',
-          //     },
-          //     {
-          //       text: 'zsh-syntax-highlighting',
-          //       link: '/terminal/zsh/zsh-syntax-highlighting/',
-          //     },
-          //   ],
-          // },
-          { text: 'im-select', link: '/terminal/im-select/' },
-        ],
-      },
-      {
-        text: '編輯器',
-        items: [
-          {
-            text: 'Neovim',
-            link: '/editor/nvim/',
-            items: [
-              {
-                text: 'packer.nvim',
-                link: '/editor/nvim/packer.nvim/',
-              },
-              {
-                text: 'fidget.nvim',
-                link: '/editor/nvim/fidget.nvim/',
-              },
-              {
-                text: 'im-select.nvim',
-                link: '/editor/nvim/im-select.nvim/',
-              },
-            ],
-          },
-        ],
-      },
+      // {
+      //   text: '桌面應用程式',
+      //   items: [
+      //     { text: 'Rectangle - 視窗管理工具', link: '/desktop/rectangle/' },
+      //   ],
+      // },
+      // {
+      //   text: '終端',
+      //   items: [
+      //     { text: 'Homebrew - 套件管理工具', link: '/terminal/homebrew/' },
+      //     { text: 'NVM - Node Version Manager', link: '/terminal/nvm/' },
+      // {
+      //   text: 'Neofetch',
+      //   link: '/terminal/neofetch/',
+      // },
+      // {
+      //   text: 'tmux - 終端管理工具',
+      //   link: '/terminal/tmux/',
+      //   items: [
+      //     {
+      //       text: 'tmuxinator - tmux sessions 管理工具',
+      //       link: '/terminal/tmux/tmuxinator/',
+      //     },
+      //     {
+      //       text: 'TPM - Tmux Plugin Manager',
+      //       link: '/terminal/tmux/tpm/',
+      //     },
+      //   ],
+      // },
+      // {
+      //   text: 'Z shell - Zsh',
+      //   link: '/terminal/zsh/',
+      //   items: [
+      //     {
+      //       text: 'Powerlevel10k',
+      //       link: '/terminal/zsh/powerlevel10k/',
+      //     },
+      //     {
+      //       text: 'zsh-autosuggestions',
+      //       link: '/terminal/zsh/zsh-autosuggestions/',
+      //     },
+      //     {
+      //       text: 'zsh-completions',
+      //       link: '/terminal/zsh/zsh-completions/',
+      //     },
+      //     {
+      //       text: 'zsh-history-substring-search',
+      //       link: '/terminal/zsh/zsh-history-substring-search/',
+      //     },
+      //     {
+      //       text: 'zsh-syntax-highlighting',
+      //       link: '/terminal/zsh/zsh-syntax-highlighting/',
+      //     },
+      //   ],
+      // },
+      //     { text: 'im-select', link: '/terminal/im-select/' },
+      //   ],
+      // },
+      // {
+      //   text: '編輯器',
+      //   items: [
+      //     {
+      //       text: 'Neovim',
+      //       link: '/editor/nvim/',
+      //       items: [
+      //         {
+      //           text: 'packer.nvim',
+      //           link: '/editor/nvim/packer.nvim/',
+      //         },
+      //         {
+      //           text: 'fidget.nvim',
+      //           link: '/editor/nvim/fidget.nvim/',
+      //         },
+      //         {
+      //           text: 'im-select.nvim',
+      //           link: '/editor/nvim/im-select.nvim/',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
     ],
   },
 };
--- docs/cheatsheet/index.md
+++ docs/cheatsheet/index.md
@@ -2,19 +2,19 @@
 
 ## Rectangle <Badge text="macOS" />
 
-<Cover src="/desktop/rectangle/assets/cover.png" />
+<Cover src="https://raw.githubusercontent.com/rxhanson/Rectangle/master/Rectangle/Assets.xcassets/AppIcon.appiconset/mac512pts2x.png" />
 
 <!-- @include: ../desktop/rectangle/sheet.md -->
 
 ## Homebrew <Badge text="macOS" />
 
 
-<Cover src="/terminal/homebrew/assets/cover.svg" />
+<Cover src="https://raw.githubusercontent.com/Homebrew/brew.sh/master/assets/img/homebrew.svg" />
 
 <!-- @include: ../terminal/homebrew/sheet.md -->
 
 ## mas-cli <Badge text="macOS" />
 
-<Cover src="/terminal/mas-cli/assets/cover.png" />
+<Cover src="https://raw.githubusercontent.com/mas-cli/mas/main/mas-cli.png" />
 
 <!-- @include: ../terminal/mas-cli/sheet.md -->

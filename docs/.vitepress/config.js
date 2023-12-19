d52bbf8c5c507d44b7e4c0796e68046614e6df47
peterhpchen <peterhsinpingchen@gmail.com>
2023-12-19 12:48:08 +0800

    feat(vim): new

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,103 @@
+const firstLayerList = [
+  'guide',
+  'rectangle',
+  'kitty',
+  'bat',
+  'curl',
+  'docker',
+  'exa',
+  'homebrew',
+  'im-select',
+  'man',
+  'mas-cli',
+  'neofetch',
+  'nvm',
+  'tmux',
+  'zsh',
+  'nvim',
+];
+const sidebar = () => {
+  return firstLayerList
+    .map((item) => {
+      return {
+        key: `/${item}/`,
+        value: [
+          { text: '介紹', link: '/guide/introduction' },
+          { text: '速查表', link: '/guide/cheatsheet' },
+          {
+            text: '桌面',
+            items: [
+              { text: 'Rectangle', link: '/rectangle/' },
+              {
+                text: 'kitty',
+                link: '/kitty/',
+                items: [{ text: 'nord-kitty', link: '/kitty/nord-kitty/' }],
+              },
+            ],
+          },
+          {
+            text: '終端',
+            items: [
+              { text: 'BAT', link: '/bat/' },
+              { text: 'curl', link: '/curl/' },
+              { text: 'docker', link: '/docker/' },
+              { text: 'exa', link: '/exa/' },
+              { text: 'Homebrew', link: '/homebrew/' },
+              { text: 'im-select', link: '/im-select/' },
+              { text: 'man', link: '/man/' },
+              { text: 'mas-cli', link: '/mas-cli/' },
+              { text: 'neofetch', link: '/neofetch/' },
+              { text: 'nvm', link: '/nvm/' },
+            ],
+          },
+          {
+            text: '終端 | tmux',
+            items: [
+              { text: 'tmux', link: '/tmux/' },
+              { text: 'tmuxinator', link: '/tmux/tmuxinator/' },
+              { text: 'TPM', link: '/tmux/tpm/' },
+            ],
+          },
+          {
+            text: '終端 | Zsh',
+            items: [
+              { text: 'Zsh', link: '/zsh/' },
+              { text: 'Powerlevel10k', link: '/zsh/powerlevel10k/' },
+              {
+                text: 'zsh-autosuggestions',
+                link: '/zsh/zsh-autosuggestions/',
+              },
+              { text: 'zsh-completions', link: '/zsh/zsh-completions/' },
+              {
+                text: 'zsh-history-substring-search',
+                link: '/zsh/zsh-history-substring-search/',
+              },
+              {
+                text: 'zsh-syntax-highlighting',
+                link: '/zsh/zsh-syntax-highlighting/',
+              },
+            ],
+          },
+          {
+            text: '編輯器 | Neovim',
+            items: [
+              { text: 'Neovim', link: '/nvim/' },
+              { text: 'fidget.nvim', link: '/nvim/fidget.nvim/' },
+              { text: 'im-select.nvim', link: '/nvim/im-select.nvim/' },
+              { text: 'packer.nvim', link: '/nvim/packer.nvim/' },
+            ],
+          },
+          { text: '候選者', link: '/guide/candidates' },
+          { text: '貢獻', link: '/guide/contributing' },
+        ],
+      };
+    })
+    .reduce((acc, cur) => {
+      acc[cur.key] = cur.value;
+      return acc;
+    }, {});
+};
+
 export default {
   title: '工具指南',
   description: '軟體開發工具的使用及配置說明',
@@ -8,74 +108,16 @@ export default {
     lineNumbers: false,
   },
   themeConfig: {
+    nav: [
+      { text: '指南', link: '/guide/cheatsheet' },
+      { text: 'Vim', link: '/vim/' },
+    ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/peterhpchen/my-dev-arms' },
     ],
-    sidebar: [
-      { text: '介紹', link: '/guide/introduction' },
-      { text: '速查表', link: '/guide/cheatsheet' },
-      {
-        text: '桌面',
-        items: [
-          { text: 'Rectangle', link: '/rectangle/' },
-          {
-            text: 'kitty',
-            link: '/kitty/',
-            items: [{ text: 'nord-kitty', link: '/kitty/nord-kitty/' }],
-          },
-        ],
-      },
-      {
-        text: '終端',
-        items: [
-          { text: 'BAT', link: '/bat/' },
-          { text: 'curl', link: '/curl/' },
-          { text: 'docker', link: '/docker/' },
-          { text: 'exa', link: '/exa/' },
-          { text: 'Homebrew', link: '/homebrew/' },
-          { text: 'im-select', link: '/im-select/' },
-          { text: 'man', link: '/man/' },
-          { text: 'mas-cli', link: '/mas-cli/' },
-          { text: 'neofetch', link: '/neofetch/' },
-          { text: 'nvm', link: '/nvm/' },
-        ],
-      },
-      {
-        text: '終端 | tmux',
-        items: [
-          { text: 'tmux', link: '/tmux/' },
-          { text: 'tmuxinator', link: '/tmux/tmuxinator/' },
-          { text: 'TPM', link: '/tmux/tpm/' },
-        ],
-      },
-      {
-        text: '終端 | Zsh',
-        items: [
-          { text: 'Zsh', link: '/zsh/' },
-          { text: 'Powerlevel10k', link: '/zsh/powerlevel10k/' },
-          { text: 'zsh-autosuggestions', link: '/zsh/zsh-autosuggestions/' },
-          { text: 'zsh-completions', link: '/zsh/zsh-completions/' },
-          {
-            text: 'zsh-history-substring-search',
-            link: '/zsh/zsh-history-substring-search/',
-          },
-          {
-            text: 'zsh-syntax-highlighting',
-            link: '/zsh/zsh-syntax-highlighting/',
-          },
-        ],
-      },
-      {
-        text: '編輯器 | Neovim',
-        items: [
-          { text: 'Neovim', link: '/nvim/' },
-          { text: 'fidget.nvim', link: '/nvim/fidget.nvim/' },
-          { text: 'im-select.nvim', link: '/nvim/im-select.nvim/' },
-          { text: 'packer.nvim', link: '/nvim/packer.nvim/' },
-        ],
-      },
-      { text: '候選者', link: '/guide/candidates' },
-      { text: '貢獻', link: '/guide/contributing' },
-    ],
+    sidebar: {
+      '/vim/': [{ text: '管理檔案', link: '/vim/manage-files' }],
+      ...sidebar(),
+    },
   },
 };
---
+++ docs/vim/manage-files.md
@@ -0,0 +1,22 @@
+# 管理檔案
+
+## Buffer
+
+開啟檔案時，檔案的內容會被 Vim 複製到 buffer 中，我們會在 window 中看到 buffer
+的內容，在 Vim 中做的任何編緝動作都是針對 buffer ，當編輯完成時，指令 `:w` 會將
+buffer 中的內容更新至檔案中。
+
+## Window
+
+Window 是用來顯示 Buffer 內容的窗口，可以在同個 window 中載入不同 buffer
+的內容，也可以在不同 window 中載入同個 buffer 。
+
+## Tab page
+
+Tab page 管控一個或多個 window ，藉由多個 Tab page 來管理不同的 window 集合。
+
+## 延伸閱讀
+
+- [Using buffers, windows, and tabs efficiently in Vim - DEV Community](https://dev.to/iggredible/using-buffers-windows-and-tabs-efficiently-in-vim-56jc)
+- [Windows - Neovim docs](https://neovim.io/doc/user/windows.html)
+- [Tabpage - Neovim docs](https://neovim.io/doc/user/tabpage.html)
